5a30746da2a35a2af78e0fac8d6c13ead8244205
aic513 <aic513@mail.ru>
2017-02-21 14:54:47 +0300

    Добавил пагинацию,вывод шаблона записи и шаблона отдельной страницы

--- wp-content/themes/theme1/footer.php
+++ wp-content/themes/theme1/footer.php
@@ -11,28 +11,7 @@
 				<h3>Виджет</h3>
 			</div>
 		<?php endif; ?>
-		<!--
-		<div class="footer-info">
-			<h3>about</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus odio, quis rutrum nibh. Etiam nec tellus malesuada odio tincidunt fringilla quis vitae nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla malesuada convallis. Morbi laoreet volutpat rhoncus.</p>
-		</div>
-		<div class="footer-info">
-			<h3>Browse</h3>
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">About Me</a></li>
-				<li><a href="#">Design Services</a></li>
-				<li><a href="#">Request Quote</a></li>
-				<li><a href="#">Advertise</a></li>
-				<li><a href="#">Contact Me</a></li>
-			</ul>
-		</div>
-		<div class="footer-info">
-			<h3>write for us</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut purus odio, quis rutrum nibh. Etiam nec tellus malesuada odio tincidunt fringilla quis vitae nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fringilla malesuada convallis. </p>
-			<p><a href="#">Contact Us Now</a></p>
-		</div>
-		-->
+
 	</div>
 </div>
 <div class="footer-copy">
